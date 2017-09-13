af8882d8c2f57040d61ce1eebc60a4552b00147a
Unknown <jjtassin@hotmail.com>
2017-09-13 16:28:36 -0300

    ABM Contacto

---
+++ backend/js/controladores/contacto.controlador.js
@@ -0,0 +1,135 @@
+
+    $(function() {
+
+        $('#login-form-link').click(function(e) {
+            $("#login-form").delay(100).fadeIn(100);
+            $("#register-form").fadeOut(100);
+            $('#register-form-link').removeClass('active');
+            $(this).addClass('active');
+            e.preventDefault();
+        });
+        $('#register-form-link').click(function(e) {
+            $("#register-form").delay(100).fadeIn(100);
+            $("#login-form").fadeOut(100);
+            $('#login-form-link').removeClass('active');
+            $(this).addClass('active');
+            e.preventDefault();
+        });
+
+    });
+    var contactos;
+
+    obtenerListado();
+
+    function obtenerListado() {
+        $('#listadoContactos').html('');
+        $('#target').html('obteniendo...');       
+        $.ajax({
+            url: 'https://aqueous-woodland-46461.herokuapp.com/api/v1/admin/contacto',
+            type: 'GET',
+            
+            dataType: "json",
+            crossDomain: true,
+            contentType:"application/json",
+            success: function (data) {
+                contactos = data;              
+              _.each(data, function(contacto){
+                $('#listadoContactos').append(' <tr>' +
+                    '<th scope="row" style="font-size: 1.5em;">1</th>' +
+                    '<td>' +contacto.nombreContacto+ '</td><td>' +contacto.mailContacto+ '</td><td class="centrarbotaccion">' +
+                    '<button onClick="mostrar(\'' + contacto._id + '\')" title="Ver" class="btn btn-default botaccion" type="button"><i style="font-size: 1.5em;" class="fa fa-eye" aria-hidden="true"></i></button>' +
+                    '<button onClick="editar(\'' + contacto._id + '\')" title="Editar" class="btn btn-default botaccion" type="button"><i style="font-size: 1.5em;" class="fa fa-pencil-square-o" aria-hidden="true"></i></button> ' +
+                    '<button title="Eliminar" onClick="eliminar(\'' + contacto._id + '\')" class="btn btn-default botaccion" type="button"><i style="font-size: 1.5em;" class="fa fa-trash" aria-hidden="true"></i> </button> ' +
+                    '</td> ' +
+                    '</tr>');
+            }) 
+          },
+          error:function(jqXHR,textStatus,errorThrown)
+          {
+              $('#target').append("jqXHR: "+jqXHR);
+              $('#target').append("textStatus: "+textStatus);
+              $('#target').append("You can not send Cross Domain AJAX requests: "+errorThrown);
+          },
+      });
+    }
+
+    function editar(idContacto){
+       var contacto =  _.find(contactos, { '_id': idContacto});
+       console.log(contacto);
+       $('#formularioAgregar').show();
+       $("#formularioAgregar :input").attr("disabled", false);
+       $("#formularioAgregar button").show();
+       $("#nombreContacto").val(contacto.nombreContacto);
+       $("#mailContacto").val(contacto.mailContacto);
+       $("#telefonoContacto").val(contacto.telefonoContacto);
+       $("#celContacto").val(contacto.celContacto);
+       $("#idContacto").val(contacto._id);
+    }
+
+    function mostrar(idContacto){
+       var contacto =  _.find(contactos, { '_id': idContacto});
+       console.log(contacto);
+       $('#formularioAgregar').show();
+       $("#formularioAgregar :input").attr("disabled", true);
+       $("#formularioAgregar button").hide();
+       $("#nombreContacto").val(contacto.nombreContacto);
+       $("#mailContacto").val(contacto.mailContacto);
+       $("#telefonoContacto").val(contacto.telefonoContacto);
+       $("#celContacto").val(contacto.celContacto);
+       $("#idContacto").val(contacto._id);
+    }
+
+    function eliminar(idContacto){
+       $.ajax({
+            url: 'https://aqueous-woodland-46461.herokuapp.com/api/v1/admin/contacto?id=' + idContacto,
+            type: 'DELETE',
+            
+            dataType: "json",
+            crossDomain: true,
+            contentType:"application/json",
+            success: function (data) {
+            },
+            error:function(jqXHR,textStatus,errorThrown)
+            {
+              obtenerListado();
+          }
+      });    
+    }
+    
+    function agregarContacto(){
+       $('#formularioAgregar').show();
+       $("#formularioAgregar :input").attr("disabled", false);
+       $("#formularioAgregar button").show();
+       $("#idContacto").val('');
+    }
+
+    function send() {
+        var isNew = $("#idContacto").val() == "";
+        var operacion = isNew ? "POST": "PUT";
+        var contacto = JSON.stringify({
+            "nombreContacto": $("#nombreContacto").val(),
+            "mailContacto":$("#mailContacto").val(),
+            "telefonoContacto":$("#telefonoContacto").val(),
+            "celContacto":$("#celContacto").val()
+        });
+
+        $('#target').html('sending..');
+        var queryParam = isNew  ? "": "?id=" + $("#idContacto").val();
+        $.ajax({
+            url: 'https://aqueous-woodland-46461.herokuapp.com/api/v1/admin/contacto' + queryParam,
+            type: operacion,
+            
+            dataType: "json",
+            crossDomain: true,
+            contentType:"application/json",
+            success: function (data) {
+                $('#formularioAgregar').hide();
+                $("#formularioAgregar :input").val('');
+                obtenerListado() ;
+            },
+            error:function(jqXHR,textStatus,errorThrown)
+            {
+          },
+          data: contacto
+      }); 
+    }
---
+++ backend/perfil/includes/footer.php
@@ -0,0 +1,34 @@
+<footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4 menufooter">
+                    <span class="titulosoporte">SOPORTE</span>
+                    <p><i class="fa fa-envelope" aria-hidden="true"></i> soporte@guiamendozagourmet.com</p>
+                    <p><i class="fa fa-phone" aria-hidden="true"></i> +54 261 4123123</p>
+                </div>
+                <div class="col-md-4 menufooter">
+                    <p></p>
+                </div>
+                <div class="col-md-4">
+
+                    <ul class="list-inline social-buttons">
+                        <li><a href="#"><i class="fa fa-twitter" style="padding-top: 10px;"></i></a>
+                        </li>
+                        <li><a href="#"><i class="fa fa-facebook" style="padding-top: 10px;"></i></a>
+                        </li>
+                        <li><a href="#"><i class="fa fa-linkedin" style="padding-top: 10px;"></i></a>
+                        </li>
+                    </ul>
+
+                    <ul class="list-inline quicklinks">
+                        <li>
+                            <span class="copyright">Copyright &copy; GMG 2017</span>
+                        </li>
+                        <li>
+                            Desarrollado por <a style="text-decoration: none;" href="#">Estudio Pronet</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </footer>
