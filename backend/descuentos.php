267916378ebe54e98c8422c703b101851e0c73e5
Unknown <jjtassin@hotmail.com>
2017-10-05 14:44:43 -0300

    popup modal al querer eliminar tipo de cocina asociado a un local

--- backend/descuentos.php
+++ backend/descuentos.php
@@ -84,7 +84,7 @@
       <div class="modal-content">
          <div class="modal-header">
         <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-            <h3>No se pudo eliminar El Descuento</h3>
+            <h3>No se pudo eliminar el Descuento</h3>
      </div>
          <div class="modal-body">
             <h5>Este Descuento tiene Locales asociados</h5>
