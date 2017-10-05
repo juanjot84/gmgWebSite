adf27492330597d9ee076253a038d01f180de60b
Unknown <jjtassin@hotmail.com>
2017-10-05 16:34:10 -0300

    Pagina de superadmin panel-administrador.php

---
+++ backend/panel-administrador.php
@@ -0,0 +1,69 @@
+<?php include("includes/head.php"); ?>
+
+<body id="page-top" class="index">
+
+
+<?php include("includes/nav.php"); ?>
+
+
+
+    <div class="container">
+        <div class="row">
+             <div class="row">
+                <div class="col-md-6 text-center" style="padding-top: 15%;">
+                    <span style="font-size: 3em;">Panel de Administración</span>
+                </div>
+                <div class="col-md-6">
+                    <img style="margin: 0 auto;" class="img-responsive" src="img/cortes.png">
+                </div>
+            </div>
+        </div>
+    </div>
+
+    
+    <?php include("includes/footer.php"); ?>
+    
+
+    <!-- jQuery -->
+    <script src="../vendor/jquery/jquery.min.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="../vendor/bootstrap/js/bootstrap.min.js"></script>
+
+    <!-- Plugin JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js" integrity="sha384-mE6eXfrb8jxl0rzJDBRanYqgBxtJ6Unn4/1F7q4xRRyIw7Vdg9jP4ycT7x1iVsgb" crossorigin="anonymous"></script>
+
+    <!-- Contact Form JavaScript -->
+    <script src="../js/jqBootstrapValidation.js"></script>
+    <script src="../js/contact_me.js"></script>
+
+    <!-- Theme JavaScript -->
+    <script src="../js/agency.min.js"></script>
+
+    <script type="text/javascript">
+        
+
+    $(function() {
+
+    $('#login-form-link').click(function(e) {
+        $("#login-form").delay(100).fadeIn(100);
+        $("#register-form").fadeOut(100);
+        $('#register-form-link').removeClass('active');
+        $(this).addClass('active');
+        e.preventDefault();
+    });
+    $('#register-form-link').click(function(e) {
+        $("#register-form").delay(100).fadeIn(100);
+        $("#login-form").fadeOut(100);
+        $('#login-form-link').removeClass('active');
+        $(this).addClass('active');
+        e.preventDefault();
+    });
+
+    });
+
+</script>
+
+</body>
+
+</html>
