5cb2515e6969d97956701f48973954f4be8374ea
Unknown <jjtassin@hotmail.com>
2017-10-05 14:27:43 -0300

    popup modal al querer eliminar medios de pagos con locales asociados

--- backend/medio-de-pago.php
+++ backend/medio-de-pago.php
@@ -74,6 +74,24 @@
                            
                         </tbody>
                     </table>
+
+<div class="modal fade" id="mostrarmodal" tabindex="-1" role="dialog" aria-labelledby="basicModal" aria-hidden="true">
+   <div class="modal-dialog">
+      <div class="modal-content">
+         <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+            <h3>No se pudo eliminar El Medio de Pago</h3>
+     </div>
+         <div class="modal-body">
+            <h5>Este Medio de Pago tiene Locales asociados</h5>
+
+     </div>
+         <div class="modal-footer">
+        <a href="#" data-dismiss="modal" class="btn btn-danger">Cerrar</a>
+     </div>
+      </div>
+   </div>
+</div>
                 </div>
             </div>
         </div>
