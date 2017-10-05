cca77700d7bd5c636fee23b45d5b73a8da3ff03e
Unknown <jjtassin@hotmail.com>
2017-10-05 14:51:32 -0300

    popup modal al eliminar tipo de negocio con negocio asociado

--- backend/js/controladores/tipoNegocio.controlador.js
+++ backend/js/controladores/tipoNegocio.controlador.js
@@ -84,7 +84,11 @@
             crossDomain: true,
             contentType:"application/json",
             success: function (data) {
-              obtenerListado();
+              if(data != 'Borrado'){  
+                $("#mostrarmodal").modal("show");
+              }else if(data == 'Borrado'){
+                 obtenerListado();
+              } 
             },
             error:function(jqXHR,textStatus,errorThrown)
             {
