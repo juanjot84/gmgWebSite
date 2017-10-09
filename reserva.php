2bb75ddb31c9a73e0d6129aa17ccce5f18fe90f2
Unknown <jjtassin@hotmail.com>
2017-10-09 17:20:18 -0300

    Nuevas paginas pasadas por Emmanuel Combes

---
+++ reserva.php
@@ -0,0 +1,245 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>Guía Mendoza Gourmet</title>
+
+  <link rel="shortcut icon" type="image/png" href="favicon.png"/>
+
+  <!-- Bootstrap Core CSS -->
+  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Custom Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+  <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+
+  <link href='https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700' rel='stylesheet' type='text/css'>
+
+  <!-- Theme CSS -->
+  <link href="css/gmgstyle.css" rel="stylesheet">
+  <link href="css/agency.min.css" rel="stylesheet">
+
+  <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+  <!--[if lt IE 9]>
+  <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js" integrity="sha384-0s5Pv64cNZJieYFkXYOTId2HMA2Lfb6q2nAcx2n0RTLUnCAoTTsS0nKEO27XyKcY" crossorigin="anonymous"></script>
+  <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js" integrity="sha384-ZoaMbDF+4LeFxg6WdScQ9nnR1QC2MIRxA1O9KWEXQwns1G8UNyIEZIQidzb0T1fo" crossorigin="anonymous"></script>
+  <![endif]-->
+
+</head>
+
+<body id="page-top" class="index">
+
+  <!-- Navigation -->
+  <nav id="mainNav" class="navbar navbar-default navbar-custom navbar-fixed-top">
+    <div class="container">
+      <!-- Brand and toggle get grouped for better mobile display -->
+      <div class="navbar-header page-scroll">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+          <span class="sr-only">Toggle navigation</span> <i class="fa fa-bars"></i>
+        </button>
+        <a class="navbar-brand page-scroll" href="#"><img class="logoweb" src="img/logo-gmg.png"></a>
+      </div>
+
+      <!-- Collect the nav links, forms, and other content for toggling -->
+      <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+        <ul class="nav navbar-nav navbar-right">
+          <li class="hidden">
+            <a href="#page-top"></a>
+          </li>
+          <li>
+            <a href="#"><i class="fa fa-user" aria-hidden="true"></i> ACCEDER</a>
+          </li>
+          <li>
+            <a href="#"><i class="fa fa-heart" style="color: #e02222 !important;" aria-hidden="true"></i> FAVORITOS</a>
+          </li>
+        </ul>
+      </div>
+      <!-- /.navbar-collapse -->
+    </div>
+    <!-- /.container-fluid -->
+  </nav>
+
+  <div class="container" style="padding-top: 7%;">
+    <div class="row">
+      <div class="col-md-12">
+        <h3 class="titulo">ZAMPA | Cocina + Barra</h3>
+        <p>Hacé tu reserva online GRATIS!</p>
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="col-md-4">
+        <p>
+          <div class="form-group">
+            <label for="sel1">¿Para cuántos adultos?</label>
+            <select class="form-control" id="sel1">
+              <option>1 adulto</option>
+              <option>2 adultos</option>
+              <option>3 adultos</option>
+              <option>4 adultos</option>
+              <option>5 adultos</option>
+              <option>6 adultos</option>
+              <option>7 adultos</option>
+              <option>8 adultos</option>
+              <option>9 adultos</option>
+              <option>10 adultos</option>
+              <option>11 adultos</option>
+              <option>12 adultos</option>
+              <option>13 adultos</option>
+              <option>14 adultos</option>
+              <option>15 adultos</option>
+              <option>16 adultos</option>
+              <option>17 adultos</option>
+              <option>18 adultos</option>
+              <option>19 adultos</option>
+              <option>20 adultos</option>
+              <option>21 adultos o más</option>
+            </select>
+          </div>
+        </p>
+      </div>
+      <div class="col-md-4">
+        <p>
+          <div class="form-group">
+            <label for="sel1">¿Para cuántos niños?</label>
+            <select class="form-control" id="sel1">
+              <option>1 niño</option>
+              <option>2 niños</option>
+              <option>3 niños</option>
+              <option>4 niños</option>
+              <option>5 niños</option>
+              <option>6 niños</option>
+              <option>7 niños</option>
+              <option>8 niños</option>
+              <option>9 niños</option>
+              <option>10 niños</option>
+              <option>11 niños</option>
+              <option>12 niños</option>
+              <option>13 niños</option>
+              <option>14 niños</option>
+              <option>15 niños</option>
+              <option>16 niños</option>
+              <option>17 niños</option>
+              <option>18 niños</option>
+              <option>19 niños</option>
+              <option>20 niños</option>
+              <option>21 niños o más</option>
+            </select>
+          </div>
+        </p>
+      </div>
+      <div class="col-md-4">
+        <p>
+          <div class="form-group">
+            <label for="sel1">¿Qué día?</label>
+            <select class="form-control" id="sel1">
+              <option>Hoy</option>
+              <option>Mañana 9</option>
+              <option>Miércoles 10</option>
+              <option>Jueves 11</option>
+            </select>
+          </div>
+      </p>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-12" style="display: inline-flex;">
+        <ul style="flex-direction: row; flex-wrap: wrap; display: flex;">
+          <li class="selechora"><button class="botonhorareserva">12:00</button></li>
+          <li class="selechora"><button class="botonhorareserva">12:00</button></li>
+          <li class="selechora"><button class="botonhorareserva">12:00</button></li>
+          <li class="selechora"><button class="botonhorareserva">12:00</button></li>
+          <li class="selechora"><button class="botonhorareserva">12:00</button></li>
+          <li class="selechora"><button class="botonhorareserva">12:00</button></li>
+          <li class="selechora"><button class="botonhorareserva">12:00</button></li>
+          <li class="selechora"><button class="botonhorareserva">12:00</button></li>
+          <li class="selechora"><button class="botonhorareserva">12:00</button></li>
+          <li class="selechora"><button class="botonhorareserva">12:00</button></li>
+          <li class="selechora"><button class="botonhorareserva">12:00</button></li>
+          <li class="selechora"><button class="botonhorareserva">12:00</button></li>
+          <li class="selechora"><button class="botonhorareserva">12:00</button></li>
+          <li class="selechora"><button class="botonhorareserva">12:00</button></li>
+          <li class="selechora"><button class="botonhorareserva">12:00</button></li>
+          <li class="selechora"><button class="botonhorareserva">12:00</button></li>
+        </ul>
+      </div>
+      <div style="text-align: center;">
+        <a href="#" class="page-scroll btn btn-xl" style="max-width: 300px; margin: 5% 0;">RESERVAR</a>
+      </div>
+    </div>
+  </div> 
+
+  <footer>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-4 menufooter">
+          <p>QUIÉNES SOMOS</p>
+          <p>TÉRMINOS Y CONDICIONES</p>
+          <p>POLÍTICAS DE PRIVACIDAD</p>
+          <p>PREGUNTAS FRECUENTES</p>
+        </div>
+        <div class="col-md-4 menufooter">
+          <p>RESTAURANTES</p>
+          <p>TAKE A W</p>
+          <p>MUNDO DULCE</p>
+          <p>DESCARGAR APP</p>
+        </div>
+        <div class="col-md-4">
+
+          <ul class="list-inline social-buttons">
+            <li><a href="#"><i class="fa fa-twitter"></i></a>
+            </li>
+            <li><a href="#"><i class="fa fa-facebook"></i></a>
+            </li>
+            <li><a href="#"><i class="fa fa-linkedin"></i></a>
+            </li>
+          </ul>
+
+          <ul class="list-inline quicklinks">
+            <li>
+              <span class="copyright">Copyright &copy; GMG 2017</span>
+            </li>
+            <li>
+              Desarrollado por <a style="text-decoration: none;" href="#">Estudio Pronet</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </footer>
+
+
+
+  <!-- jQuery -->
+  <script src="vendor/jquery/jquery.min.js"></script>
+
+  <!-- Bootstrap Core JavaScript -->
+  <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+
+  <!-- Plugin JavaScript -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js" integrity="sha384-mE6eXfrb8jxl0rzJDBRanYqgBxtJ6Unn4/1F7q4xRRyIw7Vdg9jP4ycT7x1iVsgb" crossorigin="anonymous"></script>
+
+  <!-- Contact Form JavaScript -->
+  <script src="js/jqBootstrapValidation.js"></script>
+  <script src="js/contact_me.js"></script>
+
+  <!-- Funciones de Home JavaScript -->
+  <script src="js/controladores/home.controlador.js"></script>
+
+  <!-- Theme JavaScript -->
+  <script src="js/agency.min.js"></script>
+
+  <script  src=" https://cdnjs.cloudflare.com/ajax/libs/lodash.js/4.17.4/lodash.min.js"
+  crossorigin="anonymous"></script>
+
+</body>
+
+</html>
