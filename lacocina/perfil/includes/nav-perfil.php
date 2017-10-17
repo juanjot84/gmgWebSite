4aba1b93cc3eb4744dc5e1c0dffabc6bfffd6193
Unknown <jjtassin@hotmail.com>
2017-10-17 20:04:37 -0300

    cerrar sesion desde usuarioNegocio

--- lacocina/perfil/includes/nav-perfil.php
+++ lacocina/perfil/includes/nav-perfil.php
@@ -17,7 +17,7 @@
                         <a href="#page-top"></a>
                     </li>
                     <li>
-                        <a href="scripts/cerrar_sesion.php"><i class="fa fa-user" aria-hidden="true"></i> SALIR</a>
+                        <a href="../scripts/cerrar_sesion.php"><i class="fa fa-user" aria-hidden="true"></i> SALIR</a>
                     </li>
                 </ul>
             </div>
