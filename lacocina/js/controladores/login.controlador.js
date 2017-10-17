025299ca98c4f81e79f556cd5b0be232d74104c2
Unknown <matias.kamissani@gmail.com>
2017-10-17 19:41:31 -0300

    Login
    Borro Alert

--- lacocina/js/controladores/login.controlador.js
+++ lacocina/js/controladores/login.controlador.js
@@ -37,7 +37,6 @@ function login() {
         }
       },
       error:function(jqXHR,textStatus,errorThrown){
-        alert('error');
         $("#passwordUsuario").parent().after('<span id="passwordUsuarioAlert" style="color:red"> Usuario / contraseña incorrecto</span>');
         $("#emailUsuario").addClass('alert-danger');
         $("#passwordUsuario").addClass('alert-danger');
