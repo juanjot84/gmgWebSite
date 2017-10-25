d2c8eff7cb4d72520b8fc8df36ddaa8cf9dead17
Unknown <jjtassin@hotmail.com>
2017-10-25 17:53:28 -0300

    texto en quienes-somos.php

--- quienes-somos.php
+++ quienes-somos.php
@@ -80,7 +80,7 @@
         <div>
           <h3>Sobre Guía Mendoza Gourmet</h3>
 
-          <p>Nuestra filosofía es clara: queremos ofrecer un servicio que a nosotros como usuarios nos gustaría recibir. Un sistema que resuelve la salidas de los comensales de principio a fin: desde conocer las características más importantes de cada restaurante y reservar una mesa, hasta acceder a ofertas y descuentos relevantes o inclusive recibir novedades y noticias del mundo gastronómico.</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
           </p>
         </div>
 
