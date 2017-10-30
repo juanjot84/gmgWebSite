343bd2f94bf94e85e1ebab0d3a4da9f2b30895d8
Unknown <jjtassin@hotmail.com>
2017-10-30 16:56:49 -0300

    estilos y footer

--- lacocina/includes/footer.php
+++ lacocina/includes/footer.php
@@ -2,21 +2,22 @@
         <div class="container">
             <div class="row">
                 <div class="col-md-4 menufooter">
-                    <span class="titulosoporte">SOPORTE</span>
-                    <p><i class="fa fa-envelope" aria-hidden="true"></i> soporte@guiamendozagourmet.com</p>
-                    <p><i class="fa fa-phone" aria-hidden="true"></i> +54 261 4123123</p>
+                    <img src="img/mancha-gmg.png">
                 </div>
                 <div class="col-md-4 menufooter">
-                    <p></p>
+                    <span class="titulosoporte">SOPORTE</span>
+                    <p><i class="fa fa-envelope" aria-hidden="true"></i> soporte@guiamendozagourmet.com</p>
+                    <p><i class="fa fa-phone" aria-hidden="true"></i> 0800 333 0988</p>
+                    <p><i class="fa fa-clock-o" aria-hidden="true"></i> Lunes a viernes de 9 a 18 hs.</p>
                 </div>
                 <div class="col-md-4">
 
                     <ul class="list-inline social-buttons">
-                        <li><a href="#"><i class="fa fa-twitter" style="padding-top: 10px;"></i></a>
+                        <li><a target="_blank" href="https://twitter.com/guiamendozagour"><i class="fa fa-twitter" style="padding-top: 10px;"></i></a>
                         </li>
-                        <li><a href="#"><i class="fa fa-facebook" style="padding-top: 10px;"></i></a>
+                        <li><a target="_blank" href="https://www.facebook.com/guiamendozagourmet"><i class="fa fa-facebook" style="padding-top: 10px;"></i></a>
                         </li>
-                        <li><a href="#"><i class="fa fa-linkedin" style="padding-top: 10px;"></i></a>
+                        <li><a target="_blank" href="https://www.instagram.com/guiamendozagourmet/"><i class="fa fa-instagram" style="padding-top: 10px;"></i></a>
                         </li>
                     </ul>
 
