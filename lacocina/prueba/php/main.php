afee531b819037a8451d556f087d2185adb1afcb
Unknown <jjtassin@hotmail.com>
2017-11-09 09:43:24 -0300

    php para subir imagenes al servidor

---
+++ lacocina/prueba/php/main.php
@@ -0,0 +1,19 @@
+
+<?php
+
+// En versiones de PHP anteriores a la 4.1.0, debería utilizarse $HTTP_POST_FILES en lugar
+// de $_FILES.
+
+$dir_subida = '../archivos/';
+$fichero_subido = $dir_subida . basename($_FILES['konostech']['name']);
+echo '<pre>';
+if (move_uploaded_file($_FILES['konostech']['tmp_name'], $fichero_subido)) {
+    echo "El fichero es válido y se subió con éxito.\n";
+} else {
+    echo "¡Posible ataque de subida de ficheros!\n";
+}
+echo 'Más información de depuración:';
+print_r($_FILES);
+print "</pre>";
+
+?>
