ba65ee380f9d850b6f6615bce72db83518e9d166
Unknown <emmanuel@estudiopronet.com>
2017-11-09 15:15:18 -0300

    Sección galería

---
+++ lacocina/subir-imagen.php
@@ -0,0 +1,159 @@
+<?php session_start();
+
+$idNegocio = $_SESSION['idNegocio'];
+$tipoUsuario = $_SESSION['tipoUsuario'];
+
+
+if (!$_SESSION) {
+       header('Location: index.php');
+} else {
+    if ($tipoUsuario == 'superAdmin') {
+        
+    } else {
+        header('Location: index.php');
+    }
+}
+
+?>
+<?php include("includes/head.php"); ?>
+
+<body id="page-top" class="index">
+
+
+<?php include("includes/nav.php"); ?>
+
+    
+
+    <div class="container-fluid" style="padding: 1%;background: yellow;margin-top: -21px;">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="input-group">
+                      <span class="input-group-btn">
+                      </span>
+                    </div>
+                </div>
+                <div class="col-md-6" style="text-align: right;">
+                    <div class="input-group">
+                      <span class="input-group-btn">
+                        <button class="btn btn-default" type="button" style="padding: 17px;" onClick="agregarEspecialidad()"><i class="fa fa-plus-square-o" aria-hidden="true"></i> AGREGAR NUEVO</button>
+                      </span>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+    <div class="container" style="padding-top: 2%; padding-bottom: 1%;">
+
+      <div class="panel-heading tituloseccion">Galería</div>
+        
+             <div class="row">
+
+              <div class="col-md-3">
+                <div class="miniatura-galeria">
+                  <a href="#">
+                    <img src="img/img-muestra.jpg" style="width:100%">
+                  </a>
+                </div>
+              </div>
+              <div class="col-md-3">
+                <div class="miniatura-galeria">
+                  <a href="#">
+                    <img src="img/img-muestra.jpg" style="width:100%">
+                  </a>
+                </div>
+              </div>
+              <div class="col-md-3">
+                <div class="miniatura-galeria">
+                  <a href="#">
+                    <img src="img/img-muestra.jpg" style="width:100%">
+                  </a>
+                </div>
+              </div>
+              <div class="col-md-3">
+                <div class="miniatura-galeria">
+                  <a href="#">
+                    <img src="img/img-muestra.jpg" style="width:100%">
+                  </a>
+                </div>
+              </div>
+
+            </div>
+        
+    </div>
+
+    <div class="container">
+        <div class="row">
+            <nav aria-label="Page navigation">
+              <ul class="pagination">
+                <li>
+                  <a href="#" aria-label="Previous">
+                    <span aria-hidden="true">&laquo;</span>
+                  </a>
+                </li>
+                <li><a href="#">1</a></li>
+                <li><a href="#">2</a></li>
+                <li><a href="#">3</a></li>
+                <li><a href="#">4</a></li>
+                <li><a href="#">5</a></li>
+                <li>
+                  <a href="#" aria-label="Next">
+                    <span aria-hidden="true">&raquo;</span>
+                  </a>
+                </li>
+              </ul>
+            </nav>
+        </div>
+    </div>
+
+    
+    <?php include("includes/footer.php"); ?>
+    
+
+    <!-- jQuery -->
+    <script src="../vendor/jquery/jquery.min.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="../vendor/bootstrap/js/bootstrap.min.js"></script>
+
+    <!-- Plugin JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js" integrity="sha384-mE6eXfrb8jxl0rzJDBRanYqgBxtJ6Unn4/1F7q4xRRyIw7Vdg9jP4ycT7x1iVsgb" crossorigin="anonymous"></script>
+
+    <!-- Contact Form JavaScript -->
+    <script src="../js/jqBootstrapValidation.js"></script>
+    <script src="../js/contact_me.js"></script>
+
+    <!-- Funcione de Especialidades JavaScript -->
+<script src="js/controladores/especialidades.controlador.js"></script>
+
+    <!-- Theme JavaScript -->
+    <script src="../js/agency.min.js"></script>
+
+    <script type="text/javascript">
+        
+
+    $(function() {
+
+    $('#login-form-link').click(function(e) {
+        $("#login-form").delay(100).fadeIn(100);
+        $("#register-form").fadeOut(100);
+        $('#register-form-link').removeClass('active');
+        $(this).addClass('active');
+        e.preventDefault();
+    });
+    $('#register-form-link').click(function(e) {
+        $("#register-form").delay(100).fadeIn(100);
+        $("#login-form").fadeOut(100);
+        $('#login-form-link').removeClass('active');
+        $(this).addClass('active');
+        e.preventDefault();
+    });
+
+    });
+
+</script>
+
+</body>
+
+</html>
