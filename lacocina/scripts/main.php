448e001831a2b52c29e116ea2d14efd8645c2326
Unknown <jjtassin@hotmail.com>
2017-11-09 16:34:30 -0300

    script main.php

--- lacocina/scripts/main.php
+++ lacocina/scripts/main.php
@@ -1,19 +1,16 @@
 
 <?php
 
-// En versiones de PHP anteriores a la 4.1.0, debería utilizarse $HTTP_POST_FILES en lugar
-// de $_FILES.
 
 $dir_subida = '../img-locales/';
 $fichero_subido = $dir_subida . basename($_FILES['konostech']['name']);
-echo '<pre>';
+
 if (move_uploaded_file($_FILES['konostech']['tmp_name'], $fichero_subido)) {
-    echo "El fichero es válido y se subió con éxito.\n";
+  //  echo "El fichero es válido y se subió con éxito.\n";
 } else {
     echo "¡Posible ataque de subida de ficheros!\n";
 }
-echo 'Más información de depuración:';
-print_r($_FILES);
-print "</pre>";
+
+print_r($_FILES['konostech']['name']);
 
 ?>
