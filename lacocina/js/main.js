f5c7a7f1e60ddc7c44d1c5923c3ecf54e0d0ea4a
Unknown <jjtassin@hotmail.com>
2017-11-09 22:10:37 -0300

    cambiar nombre de imagenes y crear vector a guardar

--- lacocina/js/main.js
+++ lacocina/js/main.js
@@ -1,43 +1,61 @@
 
 
+
 $(document).ready(function(){
   getArchivos();
+
 });
 
-var cont = 1;
+var imagenesLocal=[];
+
 $('#mdlArchivos').on('show.bs.modal', function (event) {
  $("#formDropZone").append("<form id='dZUpload' class='dropzone borde-dropzone' style='cursor: pointer;'>"+
  	                         "<div class='dz-default dz-message text-center'>"+
  	                           "<span><h2>Arrastra las imagenes aquí</h2></span><br>"+
  	                         "<p>(o Clic para seleccionar)</p>"+
                            "<p>Cantidad máxima 10 imágenes</p></div></form>");
-
+      
       myAwesomeDropzone = {
         url: "scripts/main.php",
         addRemoveLinks: true,
         paramName: "konostech",
         maxFilesize: 4, // MB
+        acceptedFiles: '.jpg',
         dictRemoveFile: "Eliminar",
         maxFiles: 10,
         params: {
-            parametro1: cont,
-            parametro2:'valor2'
+            idLocal: $("#idLocal").val()
         },
         success: function (file, response) {
+            
             var imgName = response;
+            var nombreImagen = imgName.trim();
+            imagenesLocal.push(nombreImagen);
             file.previewElement.classList.add("dz-success");
             console.log("Successfully uploaded :" + imgName);
+
         },
         error: function (file, response) {
           file.previewElement.classList.add("dz-error");
           myDropzone.removeFile(file);
         }
-
+        
       } // FIN myAwesomeDropzone
   var myDropzone = new Dropzone("#dZUpload", myAwesomeDropzone); 
-    myDropzone.on("complete", function(file,response) {
+    
 
-  });
+    myDropzone.on("complete", function(file) {
+       if (this.getUploadingFiles().length === 0 && this.getQueuedFiles().length === 0) {
+        console.log('todos subidos');
+        console.log(imagenesLocal)
+      } else {
+        console.log('todavia hay archivos subiendose ')
+      }
+    });
+
+   
+
+    
 });
 $('#mdlArchivos').on('hidden.bs.modal', function (event) {
   $("#formDropZone").empty();
--- lacocina/scripts/main.php
+++ lacocina/scripts/main.php
@@ -1,17 +1,21 @@
 
 <?php
 
+$fecha = uniqid();
+$idLocal = trim($_POST['idLocal']);
+$ext = '.jpg';
+$nombreImagen = $idLocal.$fecha.$ext;
 
 $dir_subida = '../img-locales/';
-$fichero_subido = $dir_subida . basename($_FILES['konostech']['name']);
+$fichero_subido = $dir_subida . basename($nombreImagen);
 
 if (move_uploaded_file($_FILES['konostech']['tmp_name'], $fichero_subido)) {
-
+       echo $nombreImagen;
 } else {
     echo "¡Posible ataque de subida de ficheros!\n";
 }
 
-print_r($_FILES['konostech']['name']);
-print_r($_POST);
+
+
 
 ?>
