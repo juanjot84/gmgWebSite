9b138cf6c4dc8d6bee05b0081ca9b544288e7bba
MaximilianoMoreno <maxem85@hotmail.com>
2017-11-13 09:20:51 -0300

    [OCASIONES, POLOS] Agregar archivos

---
+++ js/controladores/ocasiones.controlador.js
@@ -0,0 +1,55 @@
+
+function obtenerListadoOcasiones() {
+    $('.container.ocasiones').html('');
+    $('#target').html('obteniendo...');
+    $.ajax({
+        url: 'https://aqueous-woodland-46461.herokuapp.com/api/v1/admin/ocasionDestacadas',
+        type: 'GET',
+        dataType: "json",
+        crossDomain: true,
+        contentType:"application/json",
+        success: function (data) {
+          renderOcasiones(data);
+
+      },
+      error:function(jqXHR,textStatus,errorThrown)
+      {
+        console.log("jqXHR: "+jqXHR);
+        console.log("textStatus: "+textStatus);
+        console.log("You can not send Cross Domain AJAX requests: "+errorThrown);
+      }
+  });
+}
+
+function renderOcasiones(ocasiones){
+  $('#owl-demo').html('');
+  _.each(ocasiones, function(ocasion){
+    $('#owl-demo').append('' +
+      '<div class="item"  onClick="buscarOcasion(\'' + ocasion._id + '\')">' +
+        '<img class="img-responsive fotosocasiones" style="max-width:280px; " src="' + ocasion.urlImagenOcasion + '"' +
+        '<h2 class="section-heading">' + ocasion.nombreOcasion + '</h2>' +
+      '</div>');
+  });
+
+   iniciarCarousel();
+}
+
+function buscarOcasion(idOcasion) {
+  redirect('resultados-busqueda.php', 'post', idOcasion);
+}
+
+function iniciarCarousel(){
+  var owl = $("#owl-demo");
+
+  owl.owlCarousel({
+
+    autoPlay : 2000,
+    autoplayHoverPause:true,
+    items : 4, //10 items above 1000px browser width
+    itemsDesktop : [1000,5], //5 items between 1000px and 901px
+    itemsDesktopSmall : [900,3], // 3 items betweem 900px and 601px
+    itemsTablet: [600,2], //2 items between 600 and 0;
+    itemsMobile : false // itemsMobile disabled - inherit from itemsTablet option
+
+  });
+}
---
+++ js/controladores/polos.controlador.js
@@ -0,0 +1,57 @@
+
+function obtenerListadoPolos() {
+    $('.container.polos').html('');
+    $('#target').html('obteniendo...');
+    $.ajax({
+        url: 'https://aqueous-woodland-46461.herokuapp.com/api/v1/admin/polo',
+        type: 'GET',
+        dataType: "json",
+        crossDomain: true,
+        contentType:"application/json",
+        success: function (data) {
+          renderPolos(data);
+
+      },
+      error:function(jqXHR,textStatus,errorThrown)
+      {
+        console.log("jqXHR: "+jqXHR);
+        console.log("textStatus: "+textStatus);
+        console.log("You can not send Cross Domain AJAX requests: "+errorThrown);
+      }
+  });
+}
+
+function renderPolos(polos){
+  $('.container.polos').html('');
+  $('.container.polos').append('<div class="row">    <div class="col-lg-12 text-center">      <h2 class="section-heading">POLOS GASTRONÓMICOS</h2>    </div>  </div>');
+
+
+  _.each(polos, function(polo){
+    $('.container.polos').append('' +
+      '<div class="col-md-4" onClick="buscarPolo(\'' + polo._id + '\')">' +
+        '<a class="linkpolo"><h3 class="section-heading"><i id="prueba" class="fa fa-chevron-right" aria-hidden="true"></i> ' + polo.nombrePoloGastronomico + '</h3></a>' +
+      '</div>'
+    )
+  });
+
+}
+
+function buscarPolo(idOcasion) {
+  redirect('resultados-busqueda.php', 'post', 'polos', idOcasion);
+}
+
+function iniciarCarousel(){
+  var owl = $("#owl-demo");
+
+  owl.owlCarousel({
+
+    autoPlay : 2000,
+    autoplayHoverPause:true,
+    items : 4, //10 items above 1000px browser width
+    itemsDesktop : [1000,5], //5 items between 1000px and 901px
+    itemsDesktopSmall : [900,3], // 3 items betweem 900px and 601px
+    itemsTablet: [600,2], //2 items between 600 and 0;
+    itemsMobile : false // itemsMobile disabled - inherit from itemsTablet option
+
+  });
+}
