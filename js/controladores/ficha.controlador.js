abb2626f2e208289937091582023d405e2ce1995
Emma <emmanuel@estudiopronet.com>
2017-12-11 16:48:05 -0300

    Tamaño imgs miniatura Restaurantes Sugeridos v3

--- js/controladores/ficha.controlador.js
+++ js/controladores/ficha.controlador.js
@@ -375,8 +375,8 @@ function renderSugeridos(locales){
 _.each(locales, function(local){
   if(contSugeridos < 7){
   $('.container.sugeridos').append('' +
-    '<div class="col-md-2 centraimagensugeridos">' +
-    '    <a href="ficha.php?id=' + local._id + '"><img  class="sugeridos img-responsive" src="' +  local.fotoPrincipalLocal + '"> </a>' +
+    '<div class="col-md-2 ">' +
+    '    <div class="centraimagensugeridos"><a href="ficha.php?id=' + local._id + '"><img  class="sugeridos img-responsive" src="' +  local.fotoPrincipalLocal + '"> </a></div>' +
     '    <h2 class="titulosugerencia2">' + local.idNegocio.nombreNegocio + '</h2>' +
     '</div>');
     contSugeridos++;
