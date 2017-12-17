b0e3013f2708b7931308f0deb9b8f9c4954f05de
Unknown <jjtassin@hotmail.com>
2017-12-17 20:35:59 -0300

    ficha.controlador

--- js/controladores/ficha.controlador.js
+++ js/controladores/ficha.controlador.js
@@ -154,7 +154,7 @@ function popularLocal(local) {
   if(web.length < 2){
     $('#paginaNegocio').hide();
   }else{
-    $('#paginaNegocio').attr('href', web);
+$('#paginaNegocio').attr('href', web);
   }
   
   $('#telefonoLocal').text(local.telContacto);
@@ -222,7 +222,7 @@ function buscarFavoritos(local){
 
       $("#iconoFavorito").append('<h3 class="titulo"><span id="nombreNegocio">'+local.idNegocio.nombreNegocio+'</span>  <span id="bajadaNegocio">'+bajadaNegocio+'</span>'+
       '<i id="corazon" style="cursor:pointer;" class="'+iconoCorazon+'" aria-hidden="true" onClick="editarFavorito(\'' + local._id + '\',\'' + idFavorito + '\')"></i></h3>'+
-      '<p ><i class="fa fa-map-marker" aria-hidden="true"></i><span id="polo">' + local.idPoloGastronomico.nombrePoloGastronomico + '<i class="fa fa-cutlery" aria-hidden="true"></i><span class="tiponegocio">  ' +local.idTipoCocinaPrincipal.nombreTipoCocina +'</span></p>');
+      '<p ><i class="fa fa-map-marker" aria-hidden="true"></i><span id="polo">' + local.idPoloGastronomico.nombrePoloGastronomico + ' |  <i class="fa fa-cutlery" aria-hidden="true"></i><span class="tiponegocio">  ' +local.idTipoCocinaPrincipal.nombreTipoCocina +'</span></p>');
 
      },
      error:function(jqXHR,textStatus,errorThrown)
