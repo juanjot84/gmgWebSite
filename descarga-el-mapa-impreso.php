fb974b963875d99380023393973a312f00ff4318
Emma <emmanuel@estudiopronet.com>
2017-12-18 17:39:47 -0300

    Seccion descargar mapas impresos

---
+++ descarga-el-mapa-impreso.php
@@ -0,0 +1,156 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>Guía Mendoza Gourmet - Quiénes Somos</title>
+
+  <link rel="shortcut icon" type="image/png" href="favicon.png"/>
+
+  <!-- Bootstrap Core CSS -->
+  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Custom Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+  <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+
+  <link href='https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700' rel='stylesheet' type='text/css'>
+
+  <!-- Theme CSS -->
+  <link href="css/gmgstyle.css" rel="stylesheet">
+  <link href="css/agency.min.css" rel="stylesheet">
+
+  <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+  <!--[if lt IE 9]>
+  <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js" integrity="sha384-0s5Pv64cNZJieYFkXYOTId2HMA2Lfb6q2nAcx2n0RTLUnCAoTTsS0nKEO27XyKcY" crossorigin="anonymous"></script>
+  <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js" integrity="sha384-ZoaMbDF+4LeFxg6WdScQ9nnR1QC2MIRxA1O9KWEXQwns1G8UNyIEZIQidzb0T1fo" crossorigin="anonymous"></script>
+  <![endif]-->
+
+  <!-- ANALYTICS -->
+
+  <!-- Global site tag (gtag.js) - Google Analytics -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-111410422-1"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'UA-111410422-1');
+  </script>
+
+  <style type="text/css">
+    a {
+      color: #000;
+    }
+  </style>
+
+</head>
+
+<body id="page-top" class="index">
+
+  <!-- Navigation -->
+  <nav id="mainNav" class="navbar navbar-default navbar-custom navbar-fixed-top">
+    <div class="container">
+      <!-- Brand and toggle get grouped for better mobile display -->
+      <div class="navbar-header page-scroll">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+          <span class="sr-only">Toggle navigation</span> <i class="fa fa-bars"></i>
+        </button>
+        <a class="navbar-brand page-scroll" href="#"><img class="logoweb" src="img/logo-gmg.png"></a>
+      </div>
+
+      <!-- Collect the nav links, forms, and other content for toggling -->
+      <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+        <ul class="nav navbar-nav navbar-right">
+          <li class="hidden">
+            <a href="#page-top"></a>
+          </li>
+          <li>
+            <a href="#"><i class="fa fa-user" aria-hidden="true"></i> ACCEDER</a>
+          </li>
+          <li>
+            <a href="#"><i class="fa fa-heart" style="color: #e02222 !important;" aria-hidden="true"></i> FAVORITOS</a>
+          </li>
+        </ul>
+      </div>
+      <!-- /.navbar-collapse -->
+    </div>
+    <!-- /.container-fluid -->
+  </nav>
+
+
+
+  <!-- Texto Politicas -->
+  <section id="politicas" class="bg-light-gray" style="margin-top: 5%;">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-12 text-center">
+          <h2 class="section-heading">Descargá el mapa impreso</h2>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-lg-4 text-center">
+          <h3 class="elegimapa">#1 / CIUDAD • PALMARES / 2017</h3>
+          <a target="_blank" href="MapaGMG-Ciudad-Palmares.pdf"><img class="descarga-mapa" src="img/tapas-mapas/tapa-001.jpg">
+          <h3 class="botdescarmapa text-center"><i class="fa fa-map-o" aria-hidden="true"></i> Descargar</a></h3>
+        </div>
+        <div class="col-lg-4 text-center">
+          <h3 class="elegimapa">#2 / CIUDAD • PALMARES / 2017-18</h3>
+          <a target="_blank" href="MapaGMG-Ciudad-Palmares-2.pdf"><img class="descarga-mapa" src="img/tapas-mapas/tapa-002.jpg">
+          <h3 class="botdescarmapa text-center"><i class="fa fa-map-o" aria-hidden="true"></i> Descargar</a></h3>
+        </div>
+        <div class="col-lg-4 text-center">
+          <h3 class="elegimapa">#2 / LUJÁN DE CUYO • MAIPÚ / 2017-18</h3>
+          <a target="_blank" href="MapaGMG-Lujan-de-Cuyo-Maipu.pdf"><img class="descarga-mapa" src="img/tapas-mapas/tapa-003.jpg">
+          <h3 class="botdescarmapa text-center"><i class="fa fa-map-o" aria-hidden="true"></i> Descargar</a></h3>
+        </div>
+      </div>
+
+    </div>
+  </section>
+
+ 
+ <?php 
+ error_reporting(E_ERROR);
+ include("includes/footer.php"); 
+ ?>
+
+
+
+  <!-- jQuery -->
+  <script src="vendor/jquery/jquery.min.js"></script>
+
+  <!-- Bootstrap Core JavaScript -->
+  <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+
+  <!-- Plugin JavaScript -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js" integrity="sha384-mE6eXfrb8jxl0rzJDBRanYqgBxtJ6Unn4/1F7q4xRRyIw7Vdg9jP4ycT7x1iVsgb" crossorigin="anonymous"></script>
+
+  <!-- Contact Form JavaScript -->
+  <script src="js/jqBootstrapValidation.js"></script>
+  <script src="js/contact_me.js"></script>
+
+  <!-- Funciones de Home JavaScript -->
+  <script src="js/controladores/home.controlador.js"></script>
+
+  <!-- Theme JavaScript -->
+  <script src="js/agency.min.js"></script>
+
+  <script  src=" https://cdnjs.cloudflare.com/ajax/libs/lodash.js/4.17.4/lodash.min.js"
+  crossorigin="anonymous"></script>
+
+  <script>
+    obtenerListadoCocinas();
+
+  </script>
+
+</body>
+
+</html>
