4b9e0b87540ae14626eea04b64edad017fdbcaa7
Emma <emmanuel@estudiopronet.com>
2018-01-03 09:19:50 -0300

    Zendesk en home

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <meta name="description" content="">
+  <meta name="description" content="Guía Mendoza Gourmet">
   <meta name="author" content="">
 
   <title>Guía Mendoza Gourmet</title>
@@ -108,6 +108,10 @@
   /></noscript>
   <!-- End Facebook Pixel Code -->
 
+  <!-- Start of guiamendozagourmet Zendesk Widget script -->
+  <script>/*<![CDATA[*/window.zEmbed||function(e,t){var n,o,d,i,s,a=[],r=document.createElement("iframe");window.zEmbed=function(){a.push(arguments)},window.zE=window.zE||window.zEmbed,r.src="javascript:false",r.title="",r.role="presentation",(r.frameElement||r).style.cssText="display: none",d=document.getElementsByTagName("script"),d=d[d.length-1],d.parentNode.insertBefore(r,d),i=r.contentWindow,s=i.document;try{o=s}catch(e){n=document.domain,r.src='javascript:var d=document.open();d.domain="'+n+'";void(0);',o=s}o.open()._l=function(){var e=this.createElement("script");n&&(this.domain=n),e.id="js-iframe-async",e.src="https://assets.zendesk.com/embeddable_framework/main.js",this.t=+new Date,this.zendeskHost="guiamendozagourmet.zendesk.com",this.zEQueue=a,this.body.appendChild(e)},o.write('<body onload="document._l();">'),o.close()}();
+  /*]]>*/</script>
+  <!-- End of guiamendozagourmet Zendesk Widget script -->
 
 </head>
 
