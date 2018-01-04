20c002c33e7d44ac38306585a5d67e91f5eccc1a
Emma <emmanuel@estudiopronet.com>
2018-01-04 17:05:55 -0300

    Paginación eliminada en La cocina

--- lacocina/medio-de-pago.php
+++ lacocina/medio-de-pago.php
@@ -123,31 +123,7 @@ include("includes/nav.php"); ?>
         </div>
     </div>
 
-    <div class="container">
-        <div class="row">
-            <nav aria-label="Page navigation">
-              <ul class="pagination">
-                <li>
-                  <a href="#" aria-label="Previous">
-                    <span aria-hidden="true">&laquo;</span>
-                  </a>
-                </li>
-                <li><a href="#">1</a></li>
-                <li><a href="#">2</a></li>
-                <li><a href="#">3</a></li>
-                <li><a href="#">4</a></li>
-                <li><a href="#">5</a></li>
-                <li>
-                  <a href="#" aria-label="Next">
-                    <span aria-hidden="true">&raquo;</span>
-                  </a>
-                </li>
-              </ul>
-            </nav>
-        </div>
-    </div>
-
-    
+        
     <?php 
     error_reporting(E_ERROR);
     include("includes/footer.php"); ?>
