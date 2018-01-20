a307cec99864a4d25f22f88afaada4da6b8c7da8
Emma <emmanuel@estudiopronet.com>
2018-01-20 14:04:53 -0300

    Head Analytics, smart, facebook y zendesk

--- soporte.php
+++ soporte.php
@@ -26,6 +26,51 @@
   <link href="css/gmgstyle.css" rel="stylesheet">
   <link href="css/agency.min.css" rel="stylesheet">
 
+  <!-- ANALYTICS -->
+
+  <!-- Global site tag (gtag.js) - Google Analytics -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-111410422-1"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'UA-111410422-1');
+  </script>
+
+  <!-- SMARTLOOK -->
+
+  <script type="text/javascript">
+      window.smartlook||(function(d) {
+      var o=smartlook=function(){ o.api.push(arguments)},h=d.getElementsByTagName('head')[0];
+      var c=d.createElement('script');o.api=new Array();c.async=true;c.type='text/javascript';
+      c.charset='utf-8';c.src='https://rec.smartlook.com/recorder.js';h.appendChild(c);
+      })(document);
+      smartlook('init', '0f96f4e577145df7b76f73ea418d1f88a242f08b');
+  </script>
+
+  <!-- FIN SMARTLOOK -->
+
+  <!-- Facebook Pixel Code -->
+  <script>
+    !function(f,b,e,v,n,t,s)
+    {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+    n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+    if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+    n.queue=[];t=b.createElement(e);t.async=!0;
+    t.src=v;s=b.getElementsByTagName(e)[0];
+    s.parentNode.insertBefore(t,s)}(window, document,'script',
+    'https://connect.facebook.net/en_US/fbevents.js');
+    fbq('init', '510992362591893');
+    fbq('track', 'PageView');
+  </script>
+  <noscript><img height="1" width="1" style="display:none"
+    src="https://www.facebook.com/tr?id=510992362591893&ev=PageView&noscript=1"
+  /></noscript>
+  <!-- End Facebook Pixel Code -->
+
+  <?php include("includes/soportezen.php"); ?>
+
   <style type="text/css">
     body {
       background: #fff;
