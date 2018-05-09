5e3e88a25785e2a1add0542ff930b224038dbdfd
Unknown <jjtassin@hotmail.com>
2018-05-09 17:55:27 -0300

    server.js prueba

--- js/controladores/server.js
+++ js/controladores/server.js
@@ -1,5 +1,8 @@
 var server;
-if(window.location.href.indexOf("localhost")) 
-{server = "https://aqueous-woodland-46461.herokuapp.com";
-
-}else {server = "https://gmg-server.tk";}
+if(window.location.href.indexOf("localhost")){
+    server = "https://aqueous-woodland-46461.herokuapp.com"; 
+}else if (window.location.href.indexOf("demofj")){
+    server = "https://aqueous-woodland-46461.herokuapp.com"; 
+}else{
+    server = "https://gmg-server.tk";
+}
