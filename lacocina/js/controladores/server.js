9c96b66ee3443f6d1b283ab2e2248575b5ae226a
Unknown <jjtassin@hotmail.com>
2018-05-09 17:20:43 -0300

    fix cambio de color en nav de lacocina

--- lacocina/js/controladores/server.js
+++ lacocina/js/controladores/server.js
@@ -1,3 +1,5 @@
 var server;
 if(window.location.href.indexOf("localhost") || window.location.href.indexOf("demofj") ) 
-{server = "https://aqueous-woodland-46461.herokuapp.com" ;}else {server = "https://gmg-server.tk";}
+{server = "https://aqueous-woodland-46461.herokuapp.com" ;
+$("#mainNav").css('background-color', '#00ff4c');
+}else {server = "https://gmg-server.tk";}
