d83b40a200413be48f8bc86ebf844e2047dd9c0f
Unknown <jjtassin@hotmail.com>
2018-06-28 10:57:31 -0300

    Dashboard (falta chart no definido por martin)

--- lacocina/perfil/js/controladores/server.js
+++ lacocina/perfil/js/controladores/server.js
@@ -2,7 +2,7 @@ var server;
 if(window.location.href.indexOf("localhost") != -1){
     $("#mainNav").attr('style', 'background-color: #00ff4c !important');
     $(".navbar-default").attr('style', 'background-color: #00ff4c !important');
-    server = "http://localhost:3000"; 
+    server = "https://aqueous-woodland-46461.herokuapp.com"; 
 }else if (window.location.href.indexOf("demofj") != -1){
     $("#mainNav").attr('style', 'background-color: #00ff4c !important');
     $(".navbar-default").attr('style', 'background-color: #00ff4c !important');
