09f48b8a1ff236e73ee7b2549940e731dc49f59d
Unknown <arielkarlen@gmail.com>
2018-07-31 17:56:30 -0300

    Analitics Comentados

--- includes/smartlook.php
+++ includes/smartlook.php
@@ -1,4 +1,4 @@
-
+<!--
 <script type="text/javascript">
       window.smartlook||(function(d) {
       var o=smartlook=function(){ o.api.push(arguments)},h=d.getElementsByTagName('head')[0];
@@ -6,5 +6,5 @@
       c.charset='utf-8';c.src='https://rec.smartlook.com/recorder.js';h.appendChild(c);
       })(document);
       smartlook('init', '0f96f4e577145df7b76f73ea418d1f88a242f08b');
-  </script>
+  </script-->
  
