3f2e19f27637ee6129ea91e3c33467c5aacb111d
Unknown <arielkarlen@gmail.com>
2018-11-05 13:35:56 -0300

    Mapas ed. 5
    Actualizacion de descarga-el-mapa-impreso.php

--- lacocina/editar-cubiertos.php
+++ lacocina/editar-cubiertos.php
@@ -747,60 +747,6 @@
     }
   </script>
 
-<!-- Slider Range -->
-    <script>
-        var slider = document.getElementById("myRange");
-        var output = document.getElementById("demo");
-        output.innerHTML = slider.value;
-        slider.oninput = function() {
-            output.innerHTML = this.value;
-        }
-
-    </script>
-    <script>
-        var slider2 = document.getElementById("myRange2");
-        var output2 = document.getElementById("demo2");
-        output2.innerHTML = slider2.value;
-        slider2.oninput = function() {
-            output2.innerHTML = this.value;
-        }
-
-    </script>
-    
-    <!-- Script para los sliders dobles-->
-    <script>
-        function getVals() {
-            // Get slider values
-            var parent = this.parentNode;
-            var slides = parent.getElementsByTagName("input");
-            var slide1 = parseFloat(slides[0].value);
-            var slide2 = parseFloat(slides[1].value);
-            // Neither slider will clip the other, so make sure we determine which is larger
-            if (slide1 > slide2) {
-                var tmp = slide2;
-                slide2 = slide1;
-                slide1 = tmp;
-            }
-            var displayElement = parent.getElementsByClassName("rangeValues")[0];
-            displayElement.innerHTML = "<p>Entre <span>" + slide1 + "</span> y <span>" + slide2 + "<span></p>";
-        }
-        window.onload = function() {
-            // Initialize Sliders
-            var sliderSections = document.getElementsByClassName("range-slider");
-            for (var x = 0; x < sliderSections.length; x++) {
-                var sliders = sliderSections[x].getElementsByTagName("input");
-                for (var y = 0; y < sliders.length; y++) {
-                    if (sliders[y].type === "range") {
-                        sliders[y].oninput = getVals;
-                        // Manually trigger event first time to display values
-                        sliders[y].oninput();
-                    }
-                }
-            }
-        }
-
-    </script>
-
   <!-- End Slider Range -->
 
   <script>
